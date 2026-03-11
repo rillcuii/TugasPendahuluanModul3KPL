da59053459f3a1816e0c75a5a864fc65cbc63528
ariel <fadillah0777@gmail.com>
2026-03-11 15:28:45 +0800

    tambahkan fitur GUI

--- CODE_TP_MODUL3/Form1.Designer.cs
+++ CODE_TP_MODUL3/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            txtNama = new TextBox();
+            btnKirim = new Button();
+            lblOutput = new Label();
+            SuspendLayout();
+            // 
+            // txtNama
+            // 
+            txtNama.Location = new Point(339, 147);
+            txtNama.Multiline = true;
+            txtNama.Name = "txtNama";
+            txtNama.Size = new Size(166, 34);
+            txtNama.TabIndex = 0;
+            // 
+            // btnKirim
+            // 
+            btnKirim.Location = new Point(375, 218);
+            btnKirim.Name = "btnKirim";
+            btnKirim.Size = new Size(94, 29);
+            btnKirim.TabIndex = 1;
+            btnKirim.Text = "KIRIM";
+            btnKirim.UseVisualStyleBackColor = true;
+            btnKirim.Click += btnKirim_Click;
+            // 
+            // lblOutput
+            // 
+            lblOutput.AutoSize = true;
+            lblOutput.Location = new Point(339, 295);
+            lblOutput.Name = "lblOutput";
+            lblOutput.Size = new Size(146, 20);
+            lblOutput.TabIndex = 2;
+            lblOutput.Text = "Output muncul disini";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(911, 510);
+            Controls.Add(lblOutput);
+            Controls.Add(btnKirim);
+            Controls.Add(txtNama);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtNama;
+        private Button btnKirim;
+        private Label lblOutput;
     }
 }
--- CODE_TP_MODUL3/Form1.cs
+++ CODE_TP_MODUL3/Form1.cs
@@ -6,5 +6,26 @@ namespace CODE_TP_MODUL3
         {
             InitializeComponent();
         }
+
+        // Event ini akan dijalankan ketika tombol "Kirim" diklik
+        private void btnKirim_Click(object sender, EventArgs e)
+        {
+            // Mengambil input teks yang dimasukkan user pada TextBox
+            string nama = txtNama.Text;
+
+            // Mengecek apakah textbox kosong atau hanya berisi spasi
+            if (string.IsNullOrWhiteSpace(nama))
+            {
+                // Jika kosong maka label akan menampilkan pesan error
+                lblOutput.Text = "Nama tidak boleh kosong!";
+            }
+            else
+            {
+                // Jika ada input maka program menampilkan pesan halo
+                // dan menambahkan nama yang dimasukkan user
+                lblOutput.Text = "Halo " + nama;
+            }
+        }
     }
 }
+
